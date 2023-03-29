9b5e08462fe4553287672933b3764e898127852d
quynhngo2909 <dieuquynh105@gmail.com>
2023-03-29 15:43:31 +0700

    QuynhND update query sentence

--- car_parking/src/main/java/com/example/repository/parking_news/IParkingNewsRepository.java
+++ car_parking/src/main/java/com/example/repository/parking_news/IParkingNewsRepository.java
@@ -12,13 +12,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IParkingNewsRepository extends JpaRepository<ParkingNews, Integer> {
     String Get_All_Parking_News = "select pn.id as parkingNewsId, pn.title, pn.description, pn.image_url as imageUrl, pn.posting_date as postingDate" +
-            " from `parking_news` as pn" +
+            " from parking_news pn" +
             " where pn.is_deleted = 0 and" +
             " (pn.title like concat('%', :keyword '%') or pn.description like concat('%', :keyword '%') or" +
             " pn.content like concat('%', :keyword '%'))";
 
     String Find_By_Id = "select pn.id, pn.title, pn.description, pn.image_url as imageUrl ,pn.posting_date as postingDate, pn.content" +
-            " from `parking_news` as pn" +
+            " from parking_news pn" +
             " where pn.is_deleted = 0 and pn.id = :id";
 
     @Query(value = Get_All_Parking_News, countQuery = "select count(*) from parking_news", nativeQuery = true)
