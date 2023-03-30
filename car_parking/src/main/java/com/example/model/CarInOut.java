dfdcc563d2a85f4426f7f09cdc6ec892e7a2e68f
nguyenminhhoang291998 <nguyenminhhoang291998@gmail.com>
2023-03-30 12:01:48 +0700

    add

---
+++ car_parking/src/main/java/com/example/model/CarInOut.java
@@ -0,0 +1,55 @@
+package com.example.model;
+
+import javax.persistence.*;
+
+@Entity
+public class CarInOut {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "date", length = 45)
+    private String timeIn;
+
+    @Column(columnDefinition = "date", length = 45)
+    private String timeOut;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", referencedColumnName = "id")
+    private Car car;
+
+    public CarInOut() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTimeIn() {
+        return timeIn;
+    }
+
+    public void setTimeIn(String timeIn) {
+        this.timeIn = timeIn;
+    }
+
+    public String getTimeOut() {
+        return timeOut;
+    }
+
+    public void setTimeOut(String timeOut) {
+        this.timeOut = timeOut;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
---
+++ car_parking/src/main/java/com/example/model/TicketType.java
@@ -0,0 +1,50 @@
+package com.example.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class TicketType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 45, nullable = false,unique = true)
+    private String name;
+
+
+    @OneToMany(mappedBy = "ticketType")
+    @JsonIgnore
+    private Set<Ticket> ticketSet;
+
+    public TicketType() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Ticket> getTicketSet() {
+        return ticketSet;
+    }
+
+    public void setTicketSet(Set<Ticket> ticketSet) {
+        this.ticketSet = ticketSet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
