b0e26587395a35ee536784062b47d48a91765a19
quynhngo2909 <dieuquynh105@gmail.com>
2023-04-04 20:20:54 +0700

    remove

--- car_parking/src/main/java/com/example/dto/IFloorDto.java
+++
@@ -1,6 +0,0 @@
-package com.example.dto;
-
-public interface IFloorDto {
-    public long getId();
-    public String getName();
-}
--- car_parking/src/main/java/com/example/dto/ILocationOfFloor.java
+++
@@ -1,6 +0,0 @@
-package com.example.dto;
-
-public interface ILocationOfFloor {
-    public Long getId();
-    public String getName();
-}
