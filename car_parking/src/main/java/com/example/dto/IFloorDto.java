c060071bb9ab00b028f547054616d7865ab68309
ducvu1990 <you@example.com>
2023-04-05 09:04:56 +0700

    VuBD

---
+++ car_parking/src/main/java/com/example/dto/IFloorDto.java
@@ -0,0 +1,6 @@
+package com.example.dto;
+
+public interface IFloorDto {
+    public long getId();
+    public String getName();
+}
---
+++ car_parking/src/main/java/com/example/repository/IParkingNewsRepository.java
@@ -0,0 +1,30 @@
+package com.example.repository;
+
+import com.example.dto.IParkingNewsDto;
+import com.example.model.ParkingNews;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IParkingNewsRepository extends JpaRepository<ParkingNews, Integer> {
+    String Get_All_Parking_News = "select pn.parking_news_id as parkingNewsId, pn.title, pn.description, pn.image_url as imageUrl, pn.posting_date as postingDate " +
+            "from parking_news pn " +
+            "where pn.is_deleted = 0 and " +
+            "(pn.title like concat('%', :keyword '%') or pn.description like concat('%', :keyword '%') or " +
+            "pn.content like concat('%', :keyword '%'))";
+
+    String Find_By_Id = "select pn.parking_news_id as parkingNewsId, pn.title, pn.description, pn.image_url as imageUrl ,pn.posting_date as postingDate, pn.content " +
+            "from parking_news pn " +
+            "where pn.is_deleted = 0 and pn.parking_news_id = :id";
+
+    @Query(value = Get_All_Parking_News, countQuery = "select count(*) from parking_news", nativeQuery = true)
+    Page<IParkingNewsDto> findAll(Pageable pageable, @Param("keyword") String keyword);
+
+    @Query(value = Find_By_Id, nativeQuery = true)
+    IParkingNewsDto findById(@Param("id") int id);
+
+}
