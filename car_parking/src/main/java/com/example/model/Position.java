2e4d514b37a68a49d56875c051c272469f52f380
quynhngo2909 <dieuquynh105@gmail.com>
2023-04-06 17:46:49 +0700

    add

---
+++ car_parking/src/main/java/com/example/model/Position.java
@@ -0,0 +1,59 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "position")
+    @JsonIgnore
+    private Set<Employee> employeeSet;
+
+    public Position(Long id, String name, Set<Employee> employeeSet) {
+        this.id = id;
+        this.name = name;
+        this.employeeSet = employeeSet;
+    }
+
+
+    public Position() {
+    }
+
+    public Position(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployeeSet() {
+        return employeeSet;
+    }
+
+    public void setEmployeeSet(Set<Employee> employeeSet) {
+        this.employeeSet = employeeSet;
+    }
+
+}
