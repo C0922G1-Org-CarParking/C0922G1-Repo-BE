e43be6f2861bbdb46b4d71638933c6b89004b493
Huynl4 <nguyenlehuy1504@gmail.com>
2023-04-06 23:41:33 +0700

    huynl update new

---
+++ car_parking/src/main/java/com/example/repository/IParkingNewsRepository.java
@@ -0,0 +1,28 @@
+package com.example.repository;
+
+import com.example.dto.IParkingNewsDTO;
+import com.example.model.ParkingNews;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface IParkingNewsRepository extends JpaRepository<ParkingNews, Integer> {
+    String Get_All_Parking_News = "select pn.parking_news_id as parkingNewsId, pn.title, pn.description, pn.image_url as imageUrl, pn.posting_date as postingDate " +
+            "from `parking_news` pn " +
+            "where pn.is_deleted = 0 and " +
+            "(pn.title like concat('%', :keyword, '%') or pn.description like concat('%', :keyword, '%'))";
+
+    String Find_By_Id = "select pn.parking_news_id as parkingNewsId, pn.title, pn.description, pn.image_url as imageUrl ,pn.posting_date as postingDate, pn.content " +
+            "from `parking_news` pn " +
+            "where pn.is_deleted = 0 and pn.parking_news_id = :id";
+
+    @Query(value = Get_All_Parking_News, nativeQuery = true)
+    Page<IParkingNewsDTO> findAll(Pageable pageable, @Param("keyword") String keyword);
+
+    @Query(value = Find_By_Id, nativeQuery = true)
+    IParkingNewsDTO findById(@Param("id") int id);
+
+}
