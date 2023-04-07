7aaef0c89503f77752d9b825ff5e8fa78f6a54c4
unknown <tranhautanvlog.vn@gmail.com>
2023-04-07 15:49:10 +0700

    add

---
+++ car_parking/src/main/java/com/example/dto/IParkingNewsDTO.java
@@ -0,0 +1,12 @@
+package com.example.dto;
+
+import java.time.LocalDate;
+
+public interface IParkingNewsDTO {
+    Integer getParkingNewsId();
+    String getTitle();
+    String getDescription();
+    String getImageUrl();
+    String getContent();
+    LocalDate getPostingDate();
+}
---
+++ car_parking/src/main/java/com/example/model/Customer.java
@@ -0,0 +1,168 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String name;
+    @Column(columnDefinition = "date", nullable = false)
+    private String dateOfBirth;
+
+
+    public Customer(Long id, String name, String dateOfBirth, String idCard, String phoneNumber, String email, boolean gender, int district, int province, int commune, String street, Set<Car> carSet, boolean isDeleted) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.idCard = idCard;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.gender = gender;
+        this.district = district;
+        this.province = province;
+        this.commune = commune;
+        this.street = street;
+        this.carSet = carSet;
+        this.isDeleted = isDeleted;
+    }
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String idCard;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(length = 45, unique = true)
+    private String email;
+
+    private boolean gender;
+
+    @Column(nullable = false)
+    private int district;
+    @Column(nullable = false)
+    private int province;
+    @Column(nullable = false)
+    private int commune;
+    private String street;
+
+    @OneToMany(mappedBy = "customer")
+    @JsonIgnore
+    private Set<Car> carSet;
+
+    private boolean isDeleted;
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public int getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(int district) {
+        this.district = district;
+    }
+
+    public int getProvince() {
+        return province;
+    }
+
+    public void setProvince(int province) {
+        this.province = province;
+    }
+
+    public int getCommune() {
+        return commune;
+    }
+
+    public void setCommune(int commune) {
+        this.commune = commune;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Set<Car> getCarSet() {
+        return carSet;
+    }
+
+    public void setCarSet(Set<Car> carSet) {
+        this.carSet = carSet;
+    }
+
+}
